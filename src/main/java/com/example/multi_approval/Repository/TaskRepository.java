408ddd83c3c3842220fb69d26110e3833bf26420
Abhishek Jena <iamabhishekjena@gmail.com>
2025-03-08 11:04:27 +0530

    Revert "abhishek.jena|first commit"
    This reverts commit 08a8f63dbc7752eb483ba80c97aa60a5f9a9b643.

--- src/main/java/com/example/multi_approval/Repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.multi_approval.Repository;
-
-import com.example.multi_approval.Entity.Tasks;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Tasks,Integer> {
-}
